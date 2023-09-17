dbb2ccd1e1d4427adca6d8a58fca08c7bf61294b
Steven González <stevengonzalez088@gmail.com>
2023-09-17 02:16:22 -0600

    comprobacion de caracteres en menu de ayuda

--- main.asm
+++ main.asm
@@ -9,12 +9,14 @@ mensaje_menu db "	Menu de opciones:", 0Dh, 0Ah
              db "		2. Cargar Partida", 0Dh, 0Ah
              db "		3. Ayuda", 0Dh, 0Ah
              db "		4. Salir", 0Dh, 0Ah
-             db "		Elija una opcion (1-4):",0a,"$"
+             db "		Elija una opcion (1-4):",0a,0a,"$"
 
 ;;variables para apartado de ayuda
-mensaje_input_ayuda_db db "Presione 'n' para mostrar las siguientes 20 líneas o 'q' para salir.", 0Dh, "$"
-mensaje_error_linea db "No fue posible leer la Linea", 0Dh, "$"
-mensaje_lectura db "No fue posible leer el Archivo", 0Dh, "$"
+mensaje_input_ayuda_db db 0ah,"Presione 'n' para mostrar las siguientes 20 lineas o 'q' para salir.", 0a, "$"
+mensaje_salir_ayuda_db db "Presione 'q' para salir.", 0a, "$"
+mensaje_error_linea db "No fue posible leer la Linea", 0a, "$"
+mensaje_lectura db "No fue posible leer el Archivo", 0a, "$"
+mensaje_error_caracter db 0ah,"Se encontro un caracter no valido", 0a,0a,0a, "$"
 archivo_db db "AYUDA.TXT", 0
 ErrorCode dw ?                  ; Variable para almacenar el código de error
 buffer db 255 dup(0)			 ; Buffer para almacenar los caracteres leídos
@@ -95,16 +97,29 @@ ayuda:
 leer_caracter:
 	;;guardamos todo el contenido del archivo en el buffer
     mov ah, 3Fh         ; Función 3Fh: Leer desde el archivo.
-    lea dx, buffer      ; Dirección del buffer.
+    mov dx, offset buffer      ; Dirección del buffer.
     mov cx, 1	   		; leer un solo caracter.
     int 21h             ; Llamar a la interrupción 21h para leer desde el archivo.
 	jc error_lectura    ; Verificar si hubo un error al leer el archivo.
     
 	mov al, buffer[0] 		; obtener caracter a mostrar.
-	cmp al, 0Ah         	; Comprobar si es un salto de línea.
-	je aumentar_contador   	; Si es un salto de línea, mostrarlo.
-	cmp al, '$' 	  		    ; Comprobar si es el final del archivo.
+	cmp ax, 0h         	    ; Comprobar si es el final del archivo.
 	je cerrar_archivo		; Si es el final del archivo, cerrarlo.
+	cmp al, 0Ah         	; Comprobar si es un salto de línea.
+	
+	je aumentar_contador   	; Si es un salto de línea, mostrarlo.
+	cmp al, '$' 	  		; Comprobar si es el final del archivo.
+	je cerrar_archivo		; Si es el final del archivo, cerrarlo.
+
+	; verificar si el caracter es alfanumerico
+	cmp al, 0Dh 			; Comprobar si es un salto de línea.
+	je mostrar_caracter   	; Si es un salto de línea, mostrarlo.
+	cmp al, 09h 			; Comprobar si es un tab.
+	je mostrar_caracter   	; Si es un tab, mostrarlo.
+	cmp al, 20h 			; Comprobar si es un espacio.
+	jb error_char_ayuda  	; si es menor a espacio mostrar error
+	cmp al, 7Eh			    ; Comprobar si es una virgulilla.
+	ja error_char_ayuda  	; si es mayor a virgulilla mostrar error
 
 mostrar_caracter:
 	mov ah, 02h         	; Función 02h: Mostrar una caracter.
@@ -120,8 +135,13 @@ aumentar_contador:
 	jmp mostrar_caracter
 
 esperar_input:
+
+	; Mostrar un mensaje para esperar la entrada del usuario.
+	mov DX, offset mensaje_input_ayuda_db ; Dirección del mensaje.
+	mov AH, 09h							 ; Función 09h: Mostrar un mensaje.
+	int 21								 ; Llamar a la interrupción 21h para mostrar el mensaje.
     ; Esperar la entrada del usuario ('n' para siguiente, 'q' para salir).
-    mov ah, 01h         ; Función 01h: Leer un carácter desde la entrada estándar.
+    mov ah, 08h         ; Función 01h: Leer un carácter desde la entrada estándar.
     int 21h             ; Llamar a la interrupción 21h para leer la tecla presionada.
 
     cmp al, 'n'         ; Comprobar si se presionó 'n' (siguiente).
@@ -136,11 +156,23 @@ no_siguiente:
     jne esperar_input   ; Si no se presionó 'q', volver a esperar la entrada.
 	
 cerrar_archivo:
+	mov si, 0
+	; Mostrar un mensaje para esperar la entrada del usuario.
+	mov DX, offset mensaje_salir_ayuda_db ; Dirección del mensaje.
+	mov AH, 09h							 ; Función 09h: Mostrar un mensaje.
+	int 21h								 ; Llamar a la interrupción 21h para mostrar el mensaje.
+
+	; Esperar la entrada del usuario ('n' para siguiente, 'q' para salir).
+    mov ah, 01h         ; Función 01h: Leer un carácter desde la entrada estándar.
+    int 21h             ; Llamar a la interrupción 21h para leer la tecla presionada.
+
+	cmp al, 'q'         ; Comprobar si se presionó 'q' (salir).
+	jne cerrar_archivo  ; Si no se presionó 'q', volver a esperar la entrada.
     ; Cerrar el archivo y salir del programa.
     mov ah, 3Eh         ; Función 3Eh: Cerrar archivo.
     mov bx, bx          ; Descriptor de archivo.
     int 21h             ; Llamar a la interrupción 21h para cerrar el archivo.
-	jmp fin
+	jmp menu
 error:
 	; Mover el código de error a ErrorCode
     mov ErrorCode, ax
@@ -156,11 +188,24 @@ error:
     int 21h
 
 	jmp fin
+
+error_char_ayuda:
+	; Mostrar un mensaje de error si no se pudo leer el caracter.
+	mov ah, 09h         ; Función 09h: Mostrar un mensaje.
+	lea dx, mensaje_error_caracter ; Dirección del mensaje de error.
+	int 21h             ; Llamar a la interrupción 21h para mostrar el mensaje.
+
+	jmp cerrar_archivo   ; Continuar leyendo caracteres.
+
 error_lectura:
     ; Mostrar un mensaje de error si no se pudo leer el archivo.
     mov ah, 09h         ; Función 09h: Mostrar un mensaje.
     lea dx, mensaje_error_linea ; Dirección del mensaje de error.
     int 21h             ; Llamar a la interrupción 21h para mostrar el mensaje.
+
+; Macros para verificar si un carácter es alfanumérico, un dígito o un símbolo de puntuación
+
+
 fin:
 .EXIT
 END
