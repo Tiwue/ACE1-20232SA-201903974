4965b64a5535b5bf37fbf70be9ab647e85c9e6d9
Steven González <stevengonzalez088@gmail.com>
2023-09-16 22:47:10 -0600

    correccion lectura de archivo

--- main.asm
+++ main.asm
@@ -12,13 +12,15 @@ mensaje_menu db "	Menu de opciones:", 0Dh, 0Ah
              db "		Elija una opcion (1-4):",0a,"$"
 
 ;;variables para apartado de ayuda
-mensaje_db db "Presione 'n' para mostrar las siguientes 20 líneas o 'q' para salir.", 0Dh, "$"
-mensaje_error db "No fue posible leer la linea", 0Dh, "$"
-mensaje_lectura db "No fue posible leer el archivo", 0Dh, "$"
-archivo_db db "CV.TXT", 0
-buffer db 255 dup(0)   ; Buffer para almacenar una línea del archivo
-buffer_size equ 255
-lineas_por_iteracion equ 20
+mensaje_input_ayuda_db db "Presione 'n' para mostrar las siguientes 20 líneas o 'q' para salir.", 0Dh, "$"
+mensaje_error_linea db "No fue posible leer la Linea", 0Dh, "$"
+mensaje_lectura db "No fue posible leer el Archivo", 0Dh, "$"
+archivo_db db "AYUDA.TXT", 0
+ErrorCode dw ?                  ; Variable para almacenar el código de error
+buffer db 255 dup(0)			 ; Buffer para almacenar los caracteres leídos
+contadorLinea dw 0			   ; Contador de líneas
+newline db 0Dh, 0Ah, "$"
+char db ?
 
 .CODE
 .STARTUP
@@ -38,7 +40,7 @@ inicio:
 		mov AH, 09h
 		int 21
 		;;
-		mov AH, 01h
+		mov AH, 08h
 		int 21
 		;;verifica si la tecla presionada es enter
 		cmp AL, 0Dh
@@ -64,7 +66,7 @@ menu:
 		int 21
 
 		;;
-		mov AH, 01h
+		mov AH, 08h
 		int 21
 		;;verifica si la tecla presionada es 1
 		cmp AL, 31h
@@ -81,39 +83,41 @@ menu:
 		jmp menu
 ayuda:
 		; Abrir el archivo para lectura.
+		mov DX, offset archivo_db  		; Nombre del archivo a abrir.
+		mov AL, 0h          		; Modo de apertura: lectura.
 		mov AH, 3Dh         		; Función 3Dh: Abrir archivo.
-		mov AL, 0           		; Modo de apertura (0 = lectura).
-		lea DX, archivo_db  		; Nombre del archivo a abrir.
 		int 21h             		; Llamar a la interrupción 21h.
 
 		jc error            ; Verificar si hubo un error al abrir el archivo.
 
-		mov bx, ax          ; Guardar el descriptor de archivo en BX.
+		mov BX, AX          ; Guardar el descriptor de archivo en variable.
 
-		; Leer y mostrar líneas del archivo.
-		mov cx, lineas_por_iteracion ; Contador para líneas por iteración.
-		mov si, 0           ; Inicializar el índice de línea.
-
-leer_linea:
+leer_caracter:
+	;;guardamos todo el contenido del archivo en el buffer
     mov ah, 3Fh         ; Función 3Fh: Leer desde el archivo.
     lea dx, buffer      ; Dirección del buffer.
-    mov cx, buffer_size ; Tamaño del buffer.
-    mov bx, ax          ; Descriptor de archivo.
+    mov cx, 1	   		; leer un solo caracter.
     int 21h             ; Llamar a la interrupción 21h para leer desde el archivo.
+	jc error_lectura    ; Verificar si hubo un error al leer el archivo.
+    
+	mov al, buffer[0] 		; obtener caracter a mostrar.
+	cmp al, 0Ah         	; Comprobar si es un salto de línea.
+	je aumentar_contador   	; Si es un salto de línea, mostrarlo.
+	cmp al, '$' 	  		    ; Comprobar si es el final del archivo.
+	je cerrar_archivo		; Si es el final del archivo, cerrarlo.
 
-    jc error_lectura    ; Verificar si hubo un error al leer el archivo.
+mostrar_caracter:
+	mov ah, 02h         	; Función 02h: Mostrar una caracter.
+	mov dl, al          	; Caracter a mostrar.
+    int 21h             	; Llamar a la interrupción 21h para mostrar el caracter.\
 
-    ; Mostrar la línea leída.
-    mov ah, 09h         ; Función 09h: Mostrar una cadena.
-    lea dx, buffer      ; Dirección del buffer.
-    int 21h             ; Llamar a la interrupción 21h para mostrar la línea.
+    jmp leer_caracter      ; Continuar leyendo caracteres.
 
-    ; Aumentar el índice de línea y verificar si llegamos a 20.
-    inc si
-    cmp si, 14h
-    je esperar_input    ; Si llegamos a 20 líneas, esperar la entrada del usuario.
-
-    jmp leer_linea      ; Continuar leyendo más líneas del archivo.
+aumentar_contador:
+	inc SI ;incrementar el contador
+	cmp SI, 14h ;comparar si el contador es igual a 20
+	je esperar_input ;si es igual a 20 esperar la entrada del usuario
+	jmp mostrar_caracter
 
 esperar_input:
     ; Esperar la entrada del usuario ('n' para siguiente, 'q' para salir).
@@ -125,28 +129,37 @@ esperar_input:
 
     ; Reiniciar el índice de línea y volver a leer líneas.
     mov si, 0
-    jmp leer_linea
+    jmp leer_caracter
 
 no_siguiente:
     cmp al, 'q'         ; Comprobar si se presionó 'q' (salir).
     jne esperar_input   ; Si no se presionó 'q', volver a esperar la entrada.
-
+	
 cerrar_archivo:
     ; Cerrar el archivo y salir del programa.
     mov ah, 3Eh         ; Función 3Eh: Cerrar archivo.
     mov bx, bx          ; Descriptor de archivo.
     int 21h             ; Llamar a la interrupción 21h para cerrar el archivo.
-
+	jmp fin
 error:
-    ; Mostrar un mensaje de error si no se pudo abrir el archivo.
-    mov ah, 09h         ; Función 09h: Mostrar un mensaje.
-    lea dx, mensaje_error  ; Dirección del mensaje de error.
-    int 21h             ; Llamar a la interrupción 21h para mostrar el mensaje.
+	; Mover el código de error a ErrorCode
+    mov ErrorCode, ax
+
+    ; Convertir el código de error a cadena
+    mov ah, 09h
+    lea dx, ErrorCode
+	int 21h
+
+    ;Mostrar mensaje de error
+    mov ah, 09h
+    lea dx, mensaje_lectura
+    int 21h
+
 	jmp fin
 error_lectura:
     ; Mostrar un mensaje de error si no se pudo leer el archivo.
     mov ah, 09h         ; Función 09h: Mostrar un mensaje.
-    lea dx, mensaje_lectura  ; Dirección del mensaje de error.
+    lea dx, mensaje_error_linea ; Dirección del mensaje de error.
     int 21h             ; Llamar a la interrupción 21h para mostrar el mensaje.
 fin:
 .EXIT
